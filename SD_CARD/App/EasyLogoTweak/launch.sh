e4f80f23d211717efe0f87f7f4e0ebeec0c95b5d
Schmurtz <Schmurtz@gmail.com>
2023-04-19 13:38:23 +0200

    Put brightness to max for more realistic results
    - Put brightness to max for more realistic results
    - refresh thumbnails

--- SD_CARD/App/EasyLogoTweak/launch.sh
+++ SD_CARD/App/EasyLogoTweak/launch.sh
@@ -3,6 +3,7 @@ echo $0 $*
 progdir=`dirname "$0"`
 homedir=$progdir/advmenu
 
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
 infoPanel -t "Easy LogoTweak" -m "LOADING...\n \nEasy LogoTweak by Schmurtz\nMusic : The World Of Douve by DOUVE" --persistent &
 
@@ -14,7 +15,16 @@ touch /tmp/dismiss_info_panel
 
 echo "Running advancemenu now !"
 cd $homedir
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-HOME=/mnt/SDCARD/App/EasyLogoTweak/advmenu ./advmenu
-(sleep 0.5 && echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable) &
 
+# put backlight to max brightness (as during boot)
+echo 255 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+HOME=/mnt/SDCARD/App/EasyLogoTweak/advmenu ./advmenu
+
+/customer/app/sysmon freemma
+
+# restore initial backlight level
+brightness=`/customer/app/jsonval brightness`
+brightness_raw=`awk "BEGIN { print int(3 * exp(0.350656 * $brightness) + 0.5) }"`
+echo "brightness: $brightness -> $brightness_raw"
+echo $brightness_raw > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable
